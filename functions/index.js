3cde7d0427b5ffbac48cf2cc47bd9b24e4138f54
loveisgala <gabriela.gk@hotmail.com>
2022-05-02 03:04:51 -0300

    Updates

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,86 @@
-const functions = require("firebase-functions");
+ const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 admin.initializeApp();
 const db = admin.database();
 const { CloudTasksClient } = require("@google-cloud/tasks");
+
+exports.createTask = async function (payload = 'Hello, im testing!') {
+  const MAX_SCHEDULE_LIMIT = 30 * 60 * 60 * 24; // Represents 30 days in seconds.
+  const project = 'fir-test-e99ee';
+  const queue = 'new-task';
+  const location = 'us-central1';
+  const url = 'https://us-central1-fir-test-e99ee.cloudfunctions.net/taskTest';
+  const email = 'fir-test-e99ee@appspot.gserviceaccount.com';
+  const date = new Date('05/02/22 06:15'); // Intended date to schedule task
+
+  // Imports the Google Cloud Tasks firebase --debug deploylibrary.
+
+  // Instantiates a client.
+  const client = new CloudTasksClient();
+
+  // Construct the fully qualified queue name.
+  const parent = client.queuePath(project, location, queue);
+
+  // Convert message to buffer.
+  const convertedPayload = JSON.stringify(payload);
+  const body = Buffer.from(convertedPayload).toString('base64');
+
+
+  const task = {
+    httpRequest: {
+      httpMethod: 'POST',
+      url,
+      oidcToken: {
+        serviceAccountEmail: email,
+        audience: new URL(url).origin,
+      },
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body,
+    },
+  };
+
+  const convertedDate = new Date(date);
+  const currentDate = new Date();
+  console.log({ convertedDate, currentDate })
+
+  // Schedule time can not be in the past.
+  if (convertedDate < currentDate) {
+    console.error('Scheduled date in the past.');
+  } else if (convertedDate > currentDate) {
+    const date_diff_in_seconds = (convertedDate - currentDate) / 1000;
+    // Restrict schedule time to the 30 day maximum.
+    if (date_diff_in_seconds > MAX_SCHEDULE_LIMIT) {
+      console.error('Schedule time is over 30 day maximum.');
+    }
+    // Construct future date in Unix time.
+    const date_in_seconds =
+      Math.min(date_diff_in_seconds, MAX_SCHEDULE_LIMIT) + Date.now() / 1000;
+    // Add schedule time to request in Unix time using Timestamp structure.
+    // https://googleapis.dev/nodejs/tasks/latest/google.protobuf.html#.Timestamp
+    task.scheduleTime = {
+      seconds: date_in_seconds,
+    };
+  }
+
+  console.log('task.scheduleTime', task.scheduleTime.seconds)
+
+  try {
+    // Send create task request.
+    const [response] = await client.createTask({ parent, task });
+    console.log(`Created task ${response.name}`);
+    return response.name;
+  } catch (error) {
+    // Construct error for Stackdriver Error Reporting
+    console.error(Error(error.message));
+  }
+
+}
+
+this.createTask();
+
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -47,14 +125,12 @@ exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
     return snap.ref.set({ uppercase }, { merge: true });
   });
 
-
 exports.updateName = functions.https.onRequest(async (req, res) => {
-  this.createTask('hello im testing2')
   res.set('Access-Control-Allow-Origin', '*');
   // Grab the text parameter.
   const name = req.query.name;
   // Push the new message into Firestore using the Firebase Admin SDK.
-  await admin.firestore().collection('contents').doc('home').update({ name }, { merge: true });
+  const writeResult = await admin.firestore().collection('contents').doc('home').update({ name }, { merge: true });
   // Send back a message that we've successfully written the message
   res.json({ result: `Name updated to: ${name}.` });
 });
@@ -68,7 +144,6 @@ exports.getName = functions.https.onRequest((req, res) => {
   });
   // Push the new message into Firestore using the Firebase Admin SDK.
 });
-
 
 exports.notifyNameChange = functions.firestore.document('/contents/home')
   .onUpdate(async (snap, context) => {
@@ -92,84 +167,3 @@ exports.getNotifications = functions.https.onRequest((req, res) => {
     res.json(notifications);
   });
 });
-
-
-exports.createTask = async function (payload = 'Hello, im testing!') {
-  const MAX_SCHEDULE_LIMIT = 30 * 60 * 60 * 24; // Represents 30 days in seconds.
-  const project = 'fir-test-e99ee';
-  const queue = 'new-task';
-  const location = 'us-central1';
-  const url = 'https://us-central1-fir-test-e99ee.cloudfunctions.net/getTask';
-  const email = 'fir-test-e99ee@appspot.gserviceaccount.com';
-  const date = new Date('04/28/22 18:35'); // Intended date to schedule task
-
-  // Imports the Google Cloud Tasks firebase --debug deploylibrary.
-
-  // Instantiates a client.
-  const client = new CloudTasksClient();
-
-  // Construct the fully qualified queue name.
-  const parent = client.queuePath(project, location, queue);
-
-  // Convert message to buffer.
-  const convertedPayload = JSON.stringify(payload);
-  const body = Buffer.from(convertedPayload).toString('base64');
-
-
-  const task = {
-    httpRequest: {
-      httpMethod: 'POST',
-      url,
-      oidcToken: {
-        serviceAccountEmail: email,
-        audience: new URL(url).origin,
-      },
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body,
-    },
-  };
-
-  const convertedDate = new Date(date);
-  const currentDate = new Date();
-  console.log({convertedDate, currentDate})
-
-  // Schedule time can not be in the past.
-  if (convertedDate < currentDate) {
-    console.error('Scheduled date in the past.');
-  } else if (convertedDate > currentDate) {
-    const date_diff_in_seconds = (convertedDate - currentDate) / 1000;
-    // Restrict schedule time to the 30 day maximum.
-    if (date_diff_in_seconds > MAX_SCHEDULE_LIMIT) {
-      console.error('Schedule time is over 30 day maximum.');
-    }
-    // Construct future date in Unix time.
-    const date_in_seconds =
-      Math.min(date_diff_in_seconds, MAX_SCHEDULE_LIMIT) + Date.now() / 1000;
-    // Add schedule time to request in Unix time using Timestamp structure.
-    // https://googleapis.dev/nodejs/tasks/latest/google.protobuf.html#.Timestamp
-    task.scheduleTime = {
-      seconds: date_in_seconds,
-    };
-  }
-
-  console.log('task.scheduleTime', task.scheduleTime.seconds)
-
-  try {
-    // Send create task request.
-    const [response] = await client.createTask({ parent, task });
-    console.log(`Created task ${response.name}`);
-    return response.name;
-  } catch (error) {
-    // Construct error for Stackdriver Error Reporting
-    console.error(Error(error.message));
-  }
-
-}
-
-/* exports.getTask = async (req, res) => {
-  req.set('no-cors')
-  console.log('task res en index: ', res);
-}
- */
--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,175 @@
+<script>
+  export let name;
+  let nameValue = "";
+  let notifications = [];
+  async function helloWorld() {
+    const res = await fetch(
+      "http://localhost:9000/fir-test-e99ee/us-central1/helloWorld"
+    );
+    const resText = await res.text();
+    console.log("hello world", resText);
+    return resText;
+  }
+  async function byeWorld() {
+    const res = await fetch(
+      "http://localhost:9000/fir-test-e99ee/us-central1/byeWorld"
+    );
+    const resText = await res.text();
+    console.log("bye world", resText);
+    return resText;
+  }
+
+  // async function sendMessage(message) {
+  //   return await fetch('http://localhost:9000/fir-test-e99ee/us-central1/addMessage?text=' + message);
+  // }
+  helloWorld();
+  byeWorld();
+
+  async function getName() {
+    const res = await (
+      await fetch("http://localhost:9000/fir-test-e99ee/us-central1/getName")
+    ).json();
+    name = await res.name;
+  }
+  async function updateName(name) {
+    const res = await fetch(
+      "http://localhost:9000/fir-test-e99ee/us-central1/updateName?name=" + name
+    );
+    console.log(res);
+  }
+
+  async function getNotifications() {
+    const res = await (
+      await fetch(
+        "http://localhost:49833/fir-test-e99ee/us-central1/getNotifications"
+      )
+    ).json();
+    console.log("notif", res);
+    notifications = res;
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    const name = nameValue;
+    console.log("name", name);
+    updateName(name);
+  }
+  getName();
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+  <form id="update-name">
+    <input
+      type="text"
+      name="name"
+      id="name"
+      placeholder="Insert name..."
+      bind:value={nameValue}
+    />
+    <button on:click={handleSubmit}>Update name</button>
+  </form>
+
+  {#if notifications.length > 0}
+    <ul class="notifications">
+      {#each notifications as notification}
+        <li>{notification?.message}</li>
+      {/each}
+    </ul>
+  {/if}
+</main>
+
+<!-- <script>
+  export let name;
+
+  let nameValue = "";
+  let notifications = [];
+
+/*   async function helloWorld() {
+    const res = await fetch(
+      "http://localhost:5001/fir-test-e99ee/us-central1/helloWorld"
+    );
+    const resText = await res.text();
+
+    console.log("hello world", resText);
+    return resText;
+  } */
+
+/*   async function byeWorld() {
+    const res = await fetch(
+      "http://localhost:5001/fir-test-e99ee/us-central1/byeWorld"
+    );
+    const resText = await res.text();
+
+    console.log("bye world", resText);
+    return resText;
+  } */
+
+  /*   async function sendMessage(message) {
+    return await fetch('http://localhost:5001/fir-test-e99ee/us-central1/addMessage?text=' + message);    
+  } */
+
+  async function getName() {
+  const res = await (await fetch('http://localhost:5001/fir-test-e99ee/us-central1/getName')).json();
+  name = res.name;
+}
+
+  async function updateName(name) {
+    const res = await fetch(
+      "http://localhost:5000/fir-test-e99ee/us-central1/updateName?name=" + name
+    ).json();
+    console.log(res);
+  }
+
+  async function getNotifications() {
+    const res = await (
+      await fetch(
+        "http://localhost:5000/fir-test-e99ee/us-central1/getNotifications"
+      )
+    ).json();
+    console.log("notif", res);
+    notifications = res;
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const name = nameValue;
+    console.log("name", name);
+    updateName(name);
+  }
+
+  getName();
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+  <form id="update-name">
+    <input
+      type="text"
+      name="name"
+      id="name"
+      placeholder="Insert name..."
+      bind:value={nameValue}
+    />
+    <button on:click={handleSubmit}>Update name</button>
+  </form>
+
+  {#if notifications.length > 0}
+    <ul class="notifications">
+      {#each notifications as notification}
+        <li>{notification?.message}</li>
+      {/each}
+    </ul>
+  {/if}
+</main>
+
 <style>
   main {
     text-align: center;
@@ -19,11 +191,11 @@
     }
   }
 
-  .notifications { 
+  .notifications {
     list-style: none;
-    font-family: 'Courier New', Courier, monospace;
+    font-family: "Courier New", Courier, monospace;
     color: #999;
-    background: #f2f2f2;;
+    background: #f2f2f2;
     border-radius: 5px;
     border: 1px solid #ccc;
     margin: 15px;
@@ -31,85 +203,44 @@
     text-align: left;
   }
 
-  .notifications li { 
+  .notifications li {
     margin-bottom: 5px;
     font-size: 14px;
   }
-
 </style>
-
-<script>
-
-	export let name;
-
-  let nameValue = '';
-  let notifications = [];
-
-  async function helloWorld() {
-    const res = await fetch('https://us-central1-fir-test-e99ee.cloudfunctions.net/helloWorld');
-    const resText = await res.text();
-
-    console.log('hello world', resText);
-    return resText;
+ -->
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
   }
 
-  async function byeWorld() {
-    const res = await fetch('https://us-central1-fir-test-e99ee.cloudfunctions.net/byeWorld');
-    const resText = await res.text();
-
-    console.log('bye world', resText);
-    return resText;
+  .notifications {
+    list-style: none;
+    font-family: "Courier New", Courier, monospace;
+    color: #999;
+    background: #f2f2f2;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    margin: 15px;
+    padding: 15px;
+    text-align: left;
   }
-
-  async function sendMessage(message) {
-    return await fetch('http://localhost:5001/fir-test-e99ee/us-central1/addMessage?text=' + message);    
+  .notifications li {
+    margin-bottom: 5px;
+    font-size: 14px;
   }
-
-  helloWorld();
-  byeWorld();
-  
-async function getName() {
-  const res = await (await fetch('https://us-central1-fir-test-e99ee.cloudfunctions.net/getName')).json();
-  name = res.name;
-}
-
-async function updateName(name) {
-  const res = await fetch('https://us-central1-fir-test-e99ee.cloudfunctions.net/updateName?name=' + name);
-  console.log(res);
-}
-
-  
-async function getNotifications() {
-  const res = await (await fetch('https://us-central1-fir-test-e99ee.cloudfunctions.net/getNotifications')).json();
-  console.log('notif', res);
-  notifications = res;
-}
-
-function handleSubmit(e) {
-  e.preventDefault();
-  const name = nameValue;
-  console.log('name', name);
-  updateName(name);
-}
-
-getName();
-
-
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-  <form id="update-name">
-    <input type="text" name="name" id="name" placeholder="Insert name..." bind:value={nameValue} />
-    <button on:click={handleSubmit}>Update name</button>
-  </form>
-
-  {#if notifications.length > 0}
-    <ul class="notifications">
-      {#each notifications as notification}
-        <li>{notification?.message}</li>
-      {/each}
-    </ul>
-  {/if}
-</main>
+</style>
