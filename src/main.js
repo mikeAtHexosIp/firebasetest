ea409fcedf66ff64cd586e7a2da43dc069293bde
Celeste Zapata <zdceleste@gmail.com>
2022-05-12 15:30:46 -0300

    Test envs

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import App from './App.svelte';
+import App from "./App.svelte";
+import "./firebase";
+const functions = require("firebase-functions");
 
-const name = 'Wait for it...';
+const config = functions.config();
 
 const app = new App({
-	target: document.body,
-	props: {
-		name
-	}
+  target: document.body,
+  props: {
+    name: config.vars.name,
+  },
 });
 
 export default app;
